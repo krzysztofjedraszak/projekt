42e682d9c6e27a25314ea6115d9759881227846c
krzysztofjedraszak <65609128+krzysztofjedraszak@users.noreply.github.com>
2020-05-26 17:17:36 +0200

    Add files via upload

---
+++ candycrushsaga.h
@@ -0,0 +1,32 @@
+#ifndef ELEMENTSCENY_H
+#define ELEMENTSCENY_H
+
+
+class ElementSceny
+{
+public:
+    ElementSceny();
+};
+class Cukierek{
+    Cukierek();
+    void zamiana();
+    void usun();
+    void wyswietl();
+};
+class NajlepszyWynik{
+    NajlepszyWynik();
+    void wyswietl();
+    void wczytaj();
+};
+class AktualnyWynik{
+    AktualnyWynik();
+    void wyswietl();
+};
+class Plansza{
+    Plansza();
+};
+class Zegar{
+    Zegar();
+    void odliczanie();
+};
+#endif // ELEMENTSCENY_H
