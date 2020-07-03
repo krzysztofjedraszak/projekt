0b0fe4fc09b15cd48eeca4069da1be07a5ccaeaa
krzysztofjedraszak <krzysztjedr@gmail.com>
2020-07-03 16:27:28 +0200

    przed_spec

--- PROJEKT2/main.cpp
+++ PROJEKT2/main.cpp
@@ -15,19 +15,6 @@
 using namespace std;
 using namespace sf;
 
-//bool isclicked(const Vector2i mouse_position,vector<Cukierek*> &elementy ){
-
-//    for(unsigned int i=0;i<elementy.size();i++){
-//        FloatRect bounds = elementy[i]->getGlobalBounds();
-//        if(mouse_position.x >= bounds.left &&
-//           mouse_position.x <= bounds.left+bounds.width &&
-//           mouse_position.y >= bounds.top &&
-//           mouse_position.y <= bounds.top+bounds.height
-//                ){
-//            return 1;
-//        }
-//    }
-//}
 int main()
 {
     RenderWindow window(sf::VideoMode(1280, 720), "CandyCrushSaga");
